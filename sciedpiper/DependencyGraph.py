33feaee688df457c288bf6254e6edb1ad26dd05f
TimothyTickle <timothyltickle@gmail.com>
2015-09-02 11:24:44 -0400

    Moved from generators to comprehensions.

--- sciedpiper/DependencyGraph.py
+++ sciedpiper/DependencyGraph.py
@@ -24,28 +24,38 @@ class DependencyGraph( Graph.Graph ):
     Turn vertices that are terminal products.
     """
 
+    lcmd_return = []
     for vtx_terminal in self.func_get_terminal_vertices():
       if vtx_terminal.str_type == Resource.STR_TYPE:
-        yield vtx_terminal
+        lcmd_return.append(  vtx_terminal )
+    return lcmd_return
 
   # Tested
   def func_get_input_files( self ):
+    lcmd_return = []
     for vtx_input in self.func_get_graph_roots():
       if vtx_input.str_type == Resource.STR_TYPE:
-        yield vtx_input 
+        lcmd_return.append(  vtx_input )
+    return lcmd_return
 
   # Tested
   def func_get_dependencies( self ):
+    dict_cmd_return = {}
     for vtx_cur in self:
       if not ( vtx_cur.str_type == Resource.STR_TYPE ):
         continue
       if vtx_cur.func_has_child():
-        yield vtx_cur
+        dict_cmd_return.setdefault( vtx_cur.str_id, []).extend( vtx_cur.func_get_children() )
+    for str_key in dict_cmd_return.keys():
+      dict_cmd_return[ str_key ] = list( set( dict_cmd_return[ str_key ] ) )
+    return dict_cmd_return
 
   #Tested
   def func_get_products( self ):
+    lcmd_return = []
     for vtx_cur in self:
       if not ( vtx_cur.str_type ==  Resource.STR_TYPE ):
         continue
       if vtx_cur.func_has_parent() and ( not self.root in vtx_cur.func_get_parents()):
-        yield vtx_cur
+        lcmd_return.append(  vtx_cur )
+    return lcmd_return
