34cf47177af20be6d99c08665c39c88258766dcc
TimothyTickle <timothyltickle@gmail.com>
2016-07-25 11:05:14 -0400

    Added script back in

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     maintainer_email="timothyltickle@gmail.com",
     name="SciEDPipeR",
     packages=["sciedpiper"],
+    scripts=["bin/ExampleScript.py"],
     url="https://github.com/SciEDPipeR/SciEDPipeR",
     version="0.1.0"
 )
